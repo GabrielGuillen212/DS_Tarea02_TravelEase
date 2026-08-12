427b9dd55c36949450b56bca75eaa117661d54f4
GabrielGuillen212 <gaguille@espol.edu.ec>
2026-08-11 21:13:00 -0500

    Long parameter list

--- TravelEase/src/Test/NotificadorTest.java
+++ TravelEase/src/Test/NotificadorTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import Notificaciones.Notificador;
 import Notificaciones.EmailNotificado;
 import Notificaciones.AppNotificado;
+import Notificaciones.Administrador;
 
 
 class NotificadorTest {
@@ -12,10 +13,12 @@ class NotificadorTest {
     private Notificador notificador;
     private EmailNotificado usuarioemail;
     private AppNotificado usuarioapp;
+    private Administrador administrador;
 
     @BeforeEach
     public void setUp() {
         notificador = new Notificador();
+        administrador = new Administrador("Gabriel", "gaguillen@hotmail.com", "admin123", notificador);
         usuarioemail = new EmailNotificado("gaguille@espol.edu.ec");
         usuarioapp = new AppNotificado("Gaguille");
     }
@@ -44,6 +47,15 @@ class NotificadorTest {
         notificador.agregarUsuario(usuarioapp);
         notificador.notificar("Mensaje de prueba");
         assertDoesNotThrow(() -> notificador.notificar("Mensaje de prueba"), "La notificación debería enviarse sin errores.");
+    }
+
+    @Test
+    @DisplayName("Debe notificar a todos los usuarios correctamente desde el administrador")
+    void testNotificarDesdeAdministrador() {
+        notificador.agregarUsuario(usuarioemail);
+        notificador.agregarUsuario(usuarioapp);
+        administrador.gestionarPoliticas("pol123", "Cambio de política");
+        assertDoesNotThrow(() -> administrador.gestionarPoliticas("pol123", "Cambio de política"), "La notificación debería enviarse sin errores.");
     }
 
 }
