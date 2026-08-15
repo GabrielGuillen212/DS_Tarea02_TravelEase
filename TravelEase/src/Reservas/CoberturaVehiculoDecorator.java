146a8097c5b8f2b3ac32df2eb4eeefb223c75236
SJxX1511 <stejocor@espol.edu.ec>
2026-08-15 18:10:57 -0500

    arreglos
    arreglos para refactoring

--- TravelEase/src/Reservas/CoberturaVehiculoDecorator.java
+++ TravelEase/src/Reservas/CoberturaVehiculoDecorator.java
@@ -1,18 +1,9 @@
 package Reservas;
 
-public class CoberturaVehiculoDecorator extends ServicioReservaDecorator {
+public class CoberturaVehiculoDecorator
+        extends ServicioReservaDecorator {
 
     public CoberturaVehiculoDecorator(ServicioReserva servicio) {
-        super(servicio);
-    }
-
-    @Override
-    public String getDescripcion() {
-        return servicio.getDescripcion() + " + Cobertura extendida";
-    }
-
-    @Override
-    public double getPrecio() {
-        return servicio.getPrecio() + 35;
+        super(servicio, "Cobertura extendida", 35);
     }
 }
